0c6da1da2d0ca109eade4ee9c5274366dca4506e
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-24 03:59:16 -0800

    Update Login.jsx
    user save mongodb database done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -49,15 +49,23 @@ const Login = () => {
     googleLogin()
       .then((result) => {
         const loggedUser = result.user;
-        console.log(loggedUser);
-        navigate(from, { replace: true });
+navigate(from, { replace: true });
+        fetch("http://localhost:5000/user", {
+          method: "POST",
+          headers: {
+            "content-Type": "application/json",
+          },
+          body: JSON.stringify(loggedUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
       })
       .catch((error) => {
         console.log(error);
       });
   };
-
-
 
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -65,13 +73,11 @@ const Login = () => {
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           <p className="py-6 px-6">
-           Please login your email & password or google.
+            Please login your email & password or google.
           </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form
-          onSubmit={handleLogin}
-          className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -95,29 +101,31 @@ const Login = () => {
                 className="input input-bordered"
                 required
               />
-             <div className="flex justify-between">
-             <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-              <label className="label">
-                <Link
-                to='/register'
-                href="#" className="label-text-alt link link-hover underline">
-                  Do not have account? Go Register
-                </Link>
-              </label>
-             </div>
+              <div className="flex justify-between">
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+                <label className="label">
+                  <Link
+                    to="/register"
+                    href="#"
+                    className="label-text-alt link link-hover underline"
+                  >
+                    Do not have account? Go Register
+                  </Link>
+                </label>
+              </div>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
           </form>
           <div className="form-control px-8 pb-8">
-            <button
-            onClick={handleGoogleLogin}
-            className="btn btn-success">Google Login</button>
+            <button onClick={handleGoogleLogin} className="btn btn-success">
+              Google Login
+            </button>
           </div>
         </div>
       </div>
