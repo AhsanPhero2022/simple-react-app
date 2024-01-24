580e5ffaf01e023d636a8b0f3f8804714a3ed376
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-24 21:09:08 +0600

    header secoure done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import { useContext } from 'react'
+import { AuthContext } from './provider/AuthProvider'
 
 function App() {
   
-
+const {user} = useContext(AuthContext)
   return (
     <>
-    <Header></Header>
+   {
+    user ?  <Header></Header> : ""
+   }
    <Outlet></Outlet>
     </>
   )
